5e28c5a275f043705755ec08f1b453705921d619
Leonardo <peskyleo1@gmail.com>
2021-09-29 13:06:33 +0200

    👨‍💻 - Accounted for Various Screen Sizes

--- components/Segment.js
+++ components/Segment.js
@@ -9,63 +9,80 @@ import {
 import styled from "styled-components";
 import { Ionicons } from "@expo/vector-icons";
 
+const window = Dimensions.get("window");
+const screen = Dimensions.get("screen");
+
 export default function Segment() {
   const [fromPosition, setFromPosition] = useState(
     (Dimensions.get("window").width - 120) / 2
   );
-  const [distance, setDistance] = useState(new Animated.Value(fromPosition));
+  const [distance, setDistance] = useState(new Animated.Value(5));
   const [opacity, setOpacity] = useState(new Animated.Value(0));
   const [position, setPosition] = useState(0);
   const [targetValue, setTargetValue] = useState(0);
+  const [leftColor, setLeftColor] = useState("#fff");
+  const [rightColor, setRightColor] = useState("#000");
 
-  /* useEffect(() => {
-    // Anything in here is fired on component mount.
-    Animated.timing(distance, {
-      toValue: targetValue,
-      duration: 1000,
-      useNativeDriver: false,
-    }).start();
-    console.log(position);
-  }, []); */
+  const [dimensions, setDimensions] = useState({ window, screen });
 
-  const moveLeft = () => {
-    // Will change fadeAnim value to 1 in 5 seconds
-    setPosition(0);
-    //setDistance(new Animated.Value(fromPosition));
-    Animated.timing(distance, {
-      toValue: 5,
-      duration: 300,
-      useNativeDriver: false,
-    }).start();
-    console.log(position);
-  };
+  useEffect(() => {
+    const subscription = Dimensions.addEventListener(
+      "change",
+      ({ window, screen }) => {
+        setDimensions({ window, screen });
+      }
+    );
+    return () => subscription?.remove();
+  });
 
-  const moveRight = () => {
-    // Will change fadeAnim value to 1 in 5 seconds
-    setPosition(1);
-    //setDistance(new Animated.Value(0));
-    Animated.timing(distance, {
-      toValue: fromPosition,
-      duration: 300,
-      useNativeDriver: false,
-    }).start();
+  useEffect(() => {
+    // Anything in here is fired on component mount and when "position" changes
     console.log(position);
-  };
+    setFromPosition((dimensions.window.width - 120) / 2);
+    console.log("orientation Change");
+    if (position == 0) {
+      //if position is left
+      Animated.timing(distance, {
+        toValue: 5,
+        duration: 300,
+        useNativeDriver: false,
+      }).start();
+      setLeftColor("#fff");
+      setRightColor("#000");
+    } else {
+      //if position is right
+      Animated.timing(distance, {
+        toValue: fromPosition,
+        duration: 300,
+        useNativeDriver: false,
+      }).start();
+      setLeftColor("#000");
+      setRightColor("#fff");
+    }
+  }, [position, dimensions]);
 
   return (
     <Container>
       <Slider style={{ left: distance }}></Slider>
       <OptionWrapper>
-        <TouchableOpacity onPress={moveLeft}>
+        <TouchableOpacity
+          onPress={() => setPosition(0)}
+          style={{ width: "50%" }}
+        >
           <Option>
-            <Ionicons name="image" size={32} color="#fff" />
-            <Description style={{ color: "#fff" }}>On Land</Description>
+            <Ionicons name="image" size={32} color={leftColor} />
+            <Description style={{ color: leftColor }}>On Land</Description>
           </Option>
         </TouchableOpacity>
-        <TouchableOpacity onPress={moveRight}>
+        <TouchableOpacity
+          onPress={() => setPosition(1)}
+          style={{ width: "50%" }}
+        >
           <Option>
-            <Ionicons name="snow" size={32} color="#000" />
-            <Description>Beach / Snow</Description>
+            <Ionicons name="snow" size={32} color={rightColor} />
+            <Description style={{ color: rightColor }}>
+              Beach / Snow
+            </Description>
           </Option>
         </TouchableOpacity>
       </OptionWrapper>
